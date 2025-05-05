2a18e2c220879662044ccfc72faedf0a939d4e25
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-05-04 21:17:13 -0500

    Implement game logic

--- rps.js
+++ rps.js
@@ -27,3 +27,26 @@ function getHumanChoice(){
 let humanScore = 0
 let computerScore = 0
 
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        console.log("You tied! Try again!");
+    } else if (humanChoice === "rock" && computerChoice === "paper"){
+        console.log("You lose! Paper beats rock.");
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors"){
+        console.log("You lose! Scissors beats paper.");
+        computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock"){
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock"){
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You win! scissors beats scissors.");
+        humanScore++;
+    }
+}
