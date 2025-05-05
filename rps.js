95c67160d479b3a327a37a1b130d053c4a686606
Axalan-A <16419620+Axalan-A@users.noreply.github.com>
2025-05-04 20:32:25 -0500

    Link external JS file and add computer choice function

---
+++ rps.js
@@ -0,0 +1,20 @@
+function getRandomInt(max){
+    return Math.floor(Math.random() * max)
+}
+
+function getComputerChoice(){
+    const choice = getRandomInt(2);
+    let decision;
+    switch(choice){
+        case 0:
+            decision = "Rock";
+            break;
+        case 1:
+            decision = "Paper";
+            break;
+        case 2:
+            decision = "Scisors"
+            break;
+    }
+    return decision
+}
